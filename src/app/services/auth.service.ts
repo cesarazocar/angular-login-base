3b90157dfc5b7bfd8dfff80fb7c05f19e92619b8
César Azócar <cesar.azocar.pontigo@gmail.com>
2019-11-05 10:51:44 -0300

    Expiracion de token
    Nuevo diseño en home component html,
    logout en home component y redireccion al login,

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,43 +9,43 @@ import { map } from 'rxjs/operators';
 })
 export class AuthService {
 
-  private url ="http://localhost:3000"
-  userToken:string;
+  private url = "http://localhost:3000"
+  userToken: string;
 
-  
+
 
   //crear nuevo usuario
   //https://
   //Login
   //https://
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
 
     this.leerToken();
 
   }
 
-  logout(){
-
+  logout() {
+    localStorage.removeItem('token');
   }
-  login(usuario:UsuarioModel){
+  login(usuario: UsuarioModel) {
     const authData = {
       ...usuario // al ser los mismos datos  con "..." resume la informacion  (email:usuario.email, password: usuario.password)
     }
     return this.http.post(
       `${this.url}/signin`,
       authData
-    ).pipe(      
-      map((resp)=>{
+    ).pipe(
+      map((resp) => {
         console.log('Entro en el mapa del RXJS');
-        
-        this.guardarToken( resp['token']);
+
+        this.guardarToken(resp['token']);
         return resp;
       })
 
-      );
+    );
   }
-  nuevoUsuario(usuario:UsuarioModel){
+  nuevoUsuario(usuario: UsuarioModel) {
 
     const authData = {
       ...usuario // al ser los mismos datos  con "..." resume la informacion  (email:usuario.email, password: usuario.password)
@@ -53,37 +53,59 @@ export class AuthService {
     return this.http.post(
       `${this.url}/signup`,
       authData
-    ).pipe(      
-      map((resp)=>{
+    ).pipe(
+      map((resp) => {
         console.log('Entro en el mapa del RXJS');
-        
-        this.guardarToken( resp['token']);
+
+        this.guardarToken(resp['token']);
         return resp;
       })
 
-      );
+    );
   }
 
-  private guardarToken( idToken:string ){
+  private guardarToken(idToken: string) {
     this.userToken = idToken;
-    localStorage.setItem('token',idToken);
-     
+    localStorage.setItem('token', idToken);
+
+    let hoy = new Date();
+    hoy.setSeconds(3600);
+    localStorage.setItem('expira', hoy.getTime().toString());
+
   }
-  
-  leerToken(){
-    
-    if(localStorage.getItem('token')){
+
+  leerToken() {
+
+    if (localStorage.getItem('token')) {
       this.userToken = localStorage.getItem('token');
-    }else{
-      this.userToken='';
+    } else {
+      this.userToken = '';
     }
 
     return this.userToken;
   }
-  
-  estaAutenticado(): boolean{
 
+  estaAutenticado(): boolean {
     
-    return this.userToken.length > 2;
+    
+    if (this.userToken.length < 2) {
+      return false;
+    }
+
+    const expira = Number(localStorage.getItem('expira'));
+    const expiraDate = new Date();
+    expiraDate.setTime(expira);
+
+    if (expiraDate > new Date()) {
+      return true;
+    } else {
+      return false;
+    }
+
+    //return this.userToken.length > 2;
+
+
   }
+
+
 }
